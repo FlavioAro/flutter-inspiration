b688987759e00778ffdd31fcdcdb8417e6f026e0
Flavio Aro <35077695+FlavioAro@users.noreply.github.com>
2022-07-28 13:16:01 -0300

    Update search.dart

--- lib/components/search.dart
+++ lib/components/search.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class InspirationInputSearch extends StatefulWidget {
-  
   final String placeholder;
   
   const InspirationInputSearch({Key? key, required this.placeholder})
